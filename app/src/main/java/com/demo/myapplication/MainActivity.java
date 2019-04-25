d40614c1d6d94c8d958cd25e7568a2b42e4ef457
nishi <nishideveloper17@gmail.com>
2019-04-25 17:05:03 +0530

    PDF read from assert and doc file view in other App

---
+++ app/src/main/java/com/demo/myapplication/MainActivity.java
@@ -0,0 +1,115 @@
+package com.demo.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
+import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
+import com.github.barteksc.pdfviewer.listener.OnPageErrorListener;
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements OnPageChangeListener, OnLoadCompleteListener,
+        OnPageErrorListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    public static final String SAMPLE_FILE = "pdffile.pdf";
+    PDFView pdfView;
+    Integer pageNumber = 0;
+    String pdfFileName;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        pdfView = (PDFView) findViewById(R.id.pdfView);
+        displayFromAsset(SAMPLE_FILE);
+
+
+    }
+
+
+    private void displayFromAsset(String assetFileName) {
+        pdfFileName = assetFileName;
+
+        setTitle(pdfFileName);
+
+        pdfView.fromAsset(SAMPLE_FILE)
+                .defaultPage(pageNumber)
+                .onPageChange(this)
+                .enableAnnotationRendering(true)
+                .onLoad(this)
+                .scrollHandle(new DefaultScrollHandle(this))
+                .spacing(10) // in dp
+                .onPageError(this)
+                /*.pageFitPolicy(FitPolicy.BOTH)*/
+                .load();
+    }
+
+    @Override
+    public void onPageChanged(int page, int pageCount) {
+        pageNumber = page;
+        //   setTitle(String.format("%s %s / %s", pdfFileName, page + 1, pageCount));
+    }
+
+    @Override
+    public void loadComplete(int nbPages) {
+        /*PdfDocument.Meta meta = pdfView.getDocumentMeta();
+        Log.e(TAG, "title = " + meta.getTitle());
+        Log.e(TAG, "author = " + meta.getAuthor());
+        Log.e(TAG, "subject = " + meta.getSubject());
+        Log.e(TAG, "keywords = " + meta.getKeywords());
+        Log.e(TAG, "creator = " + meta.getCreator());
+        Log.e(TAG, "producer = " + meta.getProducer());
+        Log.e(TAG, "creationDate = " + meta.getCreationDate());
+        Log.e(TAG, "modDate = " + meta.getModDate());*/
+
+        // printBookmarksTree(pdfView.getTableOfContents(), "-");
+
+    }
+
+    @Override
+    public void onPageError(int page, Throwable t) {
+        Log.e(TAG, "Cannot load page " + page);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.open_doc_file_other_app:
+
+                Intent intent = new Intent(MainActivity.this, open_doc_file_other_app.class);
+                startActivity(intent);
+                //Toast.makeText(getApplicationContext(), "Item 1 Selected", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(getApplicationContext(), "Item 2 Selected", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(getApplicationContext(), "Item 3 Selected", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/demo/myapplication/open_doc_file_other_app.java
@@ -0,0 +1,235 @@
+package com.demo.myapplication;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class open_doc_file_other_app extends AppCompatActivity {
+
+    private Context context;
+    private AlertDialog.Builder builder;
+
+    protected static final int REQUEST_STORAGE_READ_ACCESS_PERMISSION = 10;
+
+    protected static final int REQUEST_STORAGE_WRITE_ACCESS_PERMISSION = 20;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_doc_file_other_app);
+
+        context = open_doc_file_other_app.this;
+        if (Build.VERSION.SDK_INT >= 23) {
+            writeFilePermissions();
+        } else {
+            copyAssets();
+        }
+    }
+
+    private void writeFilePermissions() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN
+                && ActivityCompat.checkSelfPermission(open_doc_file_other_app.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(open_doc_file_other_app.this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_STORAGE_WRITE_ACCESS_PERMISSION);
+
+        } else {
+            CreateFolder();
+        }
+    }
+
+    private void CreateFolder() {
+        File folderMain = new File(Environment.getExternalStorageDirectory() + "/PDFreader");
+
+        if (!folderMain.exists()) {
+
+            folderMain.mkdir();
+        }
+
+        copyAssets();
+
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_STORAGE_READ_ACCESS_PERMISSION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                }
+                break;
+            case REQUEST_STORAGE_WRITE_ACCESS_PERMISSION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    CreateFolder();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    private void copyAssets() {
+        AssetManager assetManager = getAssets();
+        String[] files = null;
+        String filePath = android.os.Environment.getExternalStorageDirectory().toString()
+                + "/PDFreader";
+       /* try {
+
+            files = assetManager.list(sAssets);
+        } catch (IOException e) {
+            Log.e(""error"", "Failed to get asset file list.", e);
+        }*/
+//        if (files != null) for (String filename : files) {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            in = assetManager.open("Final_Handbook_2019.doc");
+            File outFile = new File(filePath, "Final_Handbook_2019.doc");
+            out = new FileOutputStream(outFile);
+            copyFile(in, out);
+
+            openDocumentFile(filePath + "/Final_Handbook_2019.doc");
+        } catch (IOException e) {
+            Log.e("error", "Failed to copy asset file: Final_Handbook_2019.doc", e);
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    // NOOP
+                }
+            }
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    // NOOP
+                }
+            }
+        }
+//        }
+    }
+
+
+    private void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while ((read = in.read(buffer)) != -1) {
+            out.write(buffer, 0, read);
+        }
+    }
+
+    private void openDocumentFile(String filePath) {
+
+        try {
+
+            File file = new File(filePath);
+            if (file.exists()) {
+
+                boolean isAppInstalledIN_microsoft = appInstalledOrNot("com.microsoft.office.word");
+
+                boolean isAppInstalledINDocs = appInstalledOrNot("com.google.android.apps.docs.editors.docs");
+
+                if (isAppInstalledIN_microsoft || isAppInstalledINDocs) {
+
+                    Uri outputFileUri = FileProvider.getUriForFile(context, "com.demo.myapplication.fileprovider", file);
+
+                    Intent viewIntent = new Intent(Intent.ACTION_VIEW);
+
+                    viewIntent.setDataAndType(outputFileUri, "application/msword");
+
+                    viewIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+
+                    startActivity(viewIntent);
+
+                } else {
+                    Toast.makeText(context, "Install Microsoft Word Or Google Docs!", Toast.LENGTH_SHORT).show();
+
+                    builder = new AlertDialog.Builder(this);
+
+                    builder.setMessage("Do you want to install Google Docs ?")
+                            .setCancelable(false)
+                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+
+                                    final String appPackageName = "com.google.android.apps.docs.editors.docs"; // getPackageName() from Context or Activity object
+                                    try {
+                                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+                                    } catch (android.content.ActivityNotFoundException anfe) {
+                                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+                                    }
+
+                                }
+                            })
+                            .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    //  Action for 'NO' Button
+                                    dialog.cancel();
+                                    Toast.makeText(getApplicationContext(), "Microsoft Word or Google Docs app are not install in your phone.Please Install it",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                    //Creating dialog box
+                    AlertDialog alert = builder.create();
+                    //Setting the title manually
+                    alert.setTitle("Not proper source to read");
+                    alert.show();
+
+                }
+
+            } else {
+
+                Toast.makeText(context, "File not found!", Toast.LENGTH_SHORT).show();
+
+            }
+
+        } catch (Exception e) {
+            Toast.makeText(context, "Opps something wrong!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private boolean appInstalledOrNot(String uri) {
+
+        PackageManager pm = context.getPackageManager();
+
+        try {
+
+            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+
+            return true;
+
+        } catch (PackageManager.NameNotFoundException e) {
+
+        }
+
+        return false;
+
+    }
+
+}
